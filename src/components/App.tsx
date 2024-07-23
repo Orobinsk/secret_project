8b7a5821b1b9d746cc76beeea554ac25b55fa502
Orobinsk <andy_prikol@mail.ru>
2024-07-23 21:33:46 +0300

    fix links

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
-import { Link, Outlet } from 'react-router-dom';
-import { Container } from '@mui/material';
+import { Link as RouterLink, Outlet } from 'react-router-dom';
+import { Container, Link } from '@mui/material';
 
 export const App = () => {
   return (
     <Container maxWidth="lg">
-      <Link to={'/'}>main</Link>
-      <Link to={'/about'}>about</Link>
-      <Link to={'/film/1'}>film 1</Link>
+      <Link component={RouterLink} to={'/'}>
+        main
+      </Link>
+      <Link component={RouterLink} to={'/about'}>
+        about
+      </Link>
+      <Link component={RouterLink} to={'/film/1'}>
+        film 1
+      </Link>
       <Outlet />
     </Container>
   );
