410171b947eb5897c79b7c612d79e4e1cc03f6e7
Orobinsk <andy_prikol@mail.ru>
2024-10-11 23:04:40 +0300

    add theme provider

--- src/components/movieItem/MovieItem.tsx
+++ src/components/movieItem/MovieItem.tsx
@@ -1,4 +1,4 @@
-import { Box, CardMedia, IconButton, Rating, Typography } from '@mui/material';
+import { Box, CardMedia, IconButton, Rating, Theme, Typography, useTheme } from '@mui/material';
 import Card from '@mui/material/Card';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import CommentIcon from '@mui/icons-material/Comment';
@@ -7,21 +7,31 @@ import { IMovie } from '../../api/apiTypes';
 interface MovieListProps {
   movie: IMovie;
 }
+const createStyles = (theme: Theme) => ({
+  card: {
+    bgcolor: '#12161a',
+    display: 'flex',
+    borderBottom: `1px solid ${theme.palette.primary.main}`,
+    borderRadius: 0,
+  },
+  cardMedia: {
+    width: 80,
+    height: 150,
+    borderRadius: 1,
+    margin: '10px',
+    objectFit: 'cover',
+  },
+});
+
 export const MovieItem: React.FC<MovieListProps> = ({ movie }) => {
+  const theme = useTheme();
+  const styles = createStyles(theme);
+
   return (
-    <Card
-      variant="outlined"
-      sx={{
-        bgcolor: '#12161a',
-        display: 'flex',
-        borderBottom: '1px solid #89a',
-        borderRadius: 0,
-      }}
-    >
+    <Card variant="outlined" sx={styles.card}>
       <CardMedia
         component="img"
-        height="150px"
-        sx={{ width: 80, height: 150, borderRadius: 1, margin: '10px', objectFit: 'cover' }}
+        sx={styles.cardMedia}
         image={
           movie.poster_path
             ? `https://image.tmdb.org/t/p/original/${movie.poster_path}`
@@ -29,31 +39,35 @@ export const MovieItem: React.FC<MovieListProps> = ({ movie }) => {
         }
         alt={`${movie.title} Poster`}
       />
-      <Box display="flex" flexDirection="column" flexGrow="1" sx={{ p: 1 }}>
+      <Box display="flex" flexDirection="column" flexGrow="1" p={1}>
         <Box display="flex" alignItems="center">
-          <Typography color="#ffffff" fontWeight="bold" sx={{ fontSize: 29 }} gutterBottom>
+          <Typography color="primary.light" fontWeight="bold" fontSize={29} gutterBottom>
             {movie.title}
           </Typography>
-          <Typography color="#89a" sx={{ fontSize: 18, ml: 1 }} gutterBottom>
+          <Typography color="primary.main" fontSize={18} ml={1} gutterBottom>
             {movie.release_date}
           </Typography>
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+        <Box display="flex" alignItems="center" mb={1}>
           <Rating
             size="small"
             sx={{ color: 'green' }}
             value={Math.min(movie.popularity / 20, 5)}
             readOnly
           />
-          <Typography sx={{ fontSize: 14, color: '#ffffff', mr: 1 }}>{movie.popularity}</Typography>
-          <IconButton sx={{ color: 'orange' }} disableRipple>
+          <Typography color="primary.light" fontSize={14} mr={1}>
+            {movie.popularity}
+          </Typography>
+          <IconButton sx={{ color: 'orange' }}>
             <FavoriteIcon />
           </IconButton>
-          <IconButton sx={{ color: '#ffffff' }} disableRipple>
+          <IconButton sx={{ color: 'primary.light' }}>
             <CommentIcon />
           </IconButton>
         </Box>
-        <Typography sx={{ fontSize: 14, color: '#89a' }}>{movie.overview}</Typography>
+        <Typography color="primary.main" fontSize={14}>
+          {movie.overview}
+        </Typography>
       </Box>
     </Card>
   );
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from '../providers/router/Router';
 import { Suspense } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../providers/theme/theme';
 
 export const Root = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ThemeProvider>
     </Suspense>
   );
 };
