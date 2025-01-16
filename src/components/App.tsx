0021eab4905e128e11918b5908b7fc70a86b93ce
margarita <146252026+mbrita@users.noreply.github.com>
2025-01-16 15:40:01 +0300

    add footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { Nav } from './nav/nav';
+import { Footer } from './Footer/Footer';
+import { ScrollToTop } from './ScrollToTop/ScrollToTop';
 
 export const App = () => {
   return (
-    <Container maxWidth="lg" data-testid="app">
-      <Nav />
-      <Outlet />
-    </Container>
+    <>
+      <Container maxWidth="lg" data-testid="app">
+        <ScrollToTop />
+        <Nav />
+        <Outlet />
+      </Container>
+      <Footer />
+    </>
   );
 };
