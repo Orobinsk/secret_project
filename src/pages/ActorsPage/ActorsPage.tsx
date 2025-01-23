4e7eb82cd84c101387e4cbc482fad6ab4c170b0b
margarita <146252026+mbrita@users.noreply.github.com>
2025-01-23 17:10:06 +0300

    new element

--- src/pages/ActorsPage/ActorsPage.tsx
+++ src/pages/ActorsPage/ActorsPage.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Grid, Typography } from '@mui/material';
+import { Box, Container, Grid, Pagination, Typography } from '@mui/material';
 
 import { getPerson } from '../../api/api';
 import { PERSON_ENDPOINTS } from '../../api/apiTypes/apiPersonTypes';
@@ -9,6 +9,7 @@ import { PosterModal } from '../../UIKit/PosterModal/PosterModal';
 import { createActorsPageStyles } from './CreateActorsPageStyles';
 import { ImageConfig } from '../../providers/ImageConfigProvider/ImageConfigContexts';
 import { IPersonDetailsResults } from '../../types/personTypes';
+import { MovieBox } from '../../components/MovieBox/MovieBox';
 
 const PARAMS = [
   PERSON_ENDPOINTS.IMAGES,
@@ -16,6 +17,7 @@ const PARAMS = [
   PERSON_ENDPOINTS.TV_CREDITS,
 ].join(',');
 const styles = createActorsPageStyles();
+const itemsPerPage = 20;
 
 export const ActorsPage = () => {
   const { id } = useParams<{ id: string }>();
@@ -24,6 +26,11 @@ export const ActorsPage = () => {
   const [open, setOpen] = useState(false);
   const handleClickOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [page, setPage] = useState(1);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   useEffect(() => {
     const idNumber = Number(id);
@@ -34,6 +41,12 @@ export const ActorsPage = () => {
       setActor(data);
     });
   }, [id]);
+
+  const movies = actor?.movie_credits?.cast || [];
+  const totalPages = Math.ceil(movies.length / itemsPerPage);
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentMovies = movies.slice(startIndex, endIndex);
 
   const ActorImage = () => {
     const profiles = actor?.images?.profiles || [];
@@ -123,9 +136,19 @@ export const ActorsPage = () => {
       <Grid container spacing={4}>
         <Grid item xs={12} sm={4}>
           <ActorImage />
+          <ActorInfo />
         </Grid>
         <Grid item xs={12} sm={8}>
-          <ActorInfo />
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <MovieBox movie={currentMovies} />
+            <Pagination
+              count={totalPages}
+              page={page}
+              color="primary"
+              onChange={handleChange}
+              sx={{ mt: 1 }}
+            />
+          </Box>
         </Grid>
       </Grid>
     </Container>
--- src/pages/ListOfMovies/ListOfMovies.tsx
+++ src/pages/ListOfMovies/ListOfMovies.tsx
@@ -1,14 +1,14 @@
 import { Box, Grid, Pagination, PaginationItem, Typography } from '@mui/material';
 import { createListOfMoviesStyles } from './createListOfMoviesStyles';
-import { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { IResponseList } from '../../api/apiTypes/apiTypes';
 import { IMovieDiscover } from '../../types/movieTypes';
 import { getGenres, getMovieList } from '../../api/api';
-import { ImageConfig } from '../../providers/ImageConfigProvider/ImageConfigContexts';
-import { API_MOVIE_LIST_PARAMS, imageSizes, SORT_BY } from '../../constants';
-import { Link as RouterLink, useSearchParams } from 'react-router-dom';
+import { API_MOVIE_LIST_PARAMS, SORT_BY } from '../../constants';
+import { useSearchParams } from 'react-router-dom';
 import { DropDownMenu, TOption } from './DropDownMenu/DropDownMenu';
 import { IGenresDetails } from '../../api/apiTypes/apiGenresTypes';
+import { MovieBox } from '../../components/MovieBox/MovieBox';
 
 const styles = createListOfMoviesStyles();
 const DEFAULT_GENRE: IGenresDetails[] = [{ id: null, name: 'All Genres' }];
@@ -90,36 +90,10 @@ export const ListOfMovies = () => {
     );
   };
 
-  const MovieBox = ({ movieList }: { movieList?: IMovieDiscover[] }) => {
-    const imageConfig = useContext(ImageConfig);
-
-    const movieElements = movieList?.map((movie) => (
-      <Grid item xs={6} sm={3} md={2.4} lg={2.4} key={movie.id} sx={styles.posterImgStyles}>
-        <RouterLink to={`/film/${movie.id}`}>
-          {movie.poster_path ? (
-            <img
-              src={`${imageConfig.images.secure_base_url}${imageSizes.medium}${movie.poster_path}`}
-              alt={movie.title}
-              style={styles.imgStyles}
-            />
-          ) : (
-            <Box sx={styles.imgStyles}>{movie.original_title}</Box>
-          )}
-        </RouterLink>
-      </Grid>
-    ));
-
-    return (
-      <Grid item container pt={1}>
-        {movieElements}
-      </Grid>
-    );
-  };
-
   return (
     <Grid container sx={styles.wrapperStyles}>
       <Header />
-      <MovieBox movieList={movieList?.results} />
+      <MovieBox movie={movieList?.results || []} />
       <Box pt={1}>
         <Pagination
           count={movieList?.total_pages}
